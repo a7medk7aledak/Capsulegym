952827839ef23798cf0d31e605a86e1fd90e601b
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-06 21:13:05 +0300

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,63 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      telephone,
+      age,
+      country,
+      gender,
+    } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      telephone,
+      age,
+      country,
+      gender,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+// The login function remains the same
+exports.login = async (req, res) => {
+  // ... (keep the existing login function)
+};

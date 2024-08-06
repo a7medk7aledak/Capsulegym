7aa6e9e3cfaf2130d72a92c00e799406b4c3cf34
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-06 21:13:11 +0300

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    telephone: {
+      type: Number,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("customer", UserSchema);
